b5191bec0e8622d8e63fadcf2531171e90759d50
mahendra-918 <kasulamahi624@gmail.com>
2025-05-13 12:53:52 +0530

    changed offset and userlist

--- lib/app/utils/share_dialog.dart
+++ lib/app/utils/share_dialog.dart
@@ -57,45 +57,16 @@ class ShareDialog extends StatelessWidget {
                                 controller.selectedEmails,
                               );
                             } else {
-                              // Get the user IDs from emails
-                              List<String> sharedUserIds = await FirestoreDb.getUserIdsByEmails(
-                                controller.selectedEmails,
-                              );
-                              
-                              // Update the controller's sharedUserIds list
-                              if (sharedUserIds.isNotEmpty) {
-                                // Make sure we don't add duplicates
-                                for (String userId in sharedUserIds) {
-                                  if (!controller.sharedUserIds.contains(userId)) {
-                                    controller.sharedUserIds.add(userId);
-                                  }
-                                }
-                                
-                                // Update the alarm record
-                                controller.alarmRecord.value.sharedUserIds = controller.sharedUserIds;
-                                
-                                // Show the shared users list
-                                controller.showSharedUsersList.value = true;
-                              }
-                              
-                              // Share the alarm
                               await FirestoreDb.shareAlarm(
                                 controller.selectedEmails,
                                 controller.alarmRecord.value,
                               );
                               
-                              // Send notifications
-                              await PushNotifications().triggerSharedItemNotification(sharedUserIds);
-                              
-                              // Close the dialog
-                              Get.back();
-                              
-                              // Show success message
-                              Get.snackbar(
-                                'Shared Successfully',
-                                'Alarm has been shared with ${controller.selectedEmails.length} users',
-                                snackPosition: SnackPosition.BOTTOM,
+                            List<String> sharedUserIds = await FirestoreDb.getUserIdsByEmails(
+                                controller.selectedEmails,
                               );
+
+                              await PushNotifications().triggerSharedItemNotification(sharedUserIds);
                             }
                           } else {
                             Get.snackbar('Error', 'Select an User');
